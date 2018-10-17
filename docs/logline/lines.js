335eb7fd8892a2ff0ab49045362615df4235f46a
vxqqb <sun.yi_sh@qq.com>
2018-10-17 17:00:35 +0800

    modi loglines

--- docs/logline/lines.js
+++ docs/logline/lines.js
@@ -1,15 +1,23 @@
-var stringify = (date) => {
+var stringify = (date, noMiniSec) => {
     let H = date.getHours()
+    H = H < 10 ? '0' + H : H
     let M = date.getMinutes()
+    M = M < 10 ? '0' + M : M
     let S = date.getSeconds()
+    S = S < 10 ? '0' + S : S
     let ss = date % 1000
-    return `${H}:${M}:${S}.${ss}`
+    return `${H}${':' + M}${':' + S}${noMiniSec ? '' : ('.' + ss)}`
 }
 const DAY_START = 1539619200000
 const DAY_SPAN = 86400000
-const LOG_LINE_COUNT = 50
-const HIGH_LIGHT_LOG_LINE_COUNT = 5
-const LOGLINE_SECTION_COUNT = 5 // 整条bar分5段展示时间刻度
+const LOG_LINE_COUNT = 10
+const HIGH_LIGHT_LOG_LINE_COUNT = 3
+let LOGLINE_SECTION_COUNT = 5 // 整条bar分5段展示时间刻度
+if (LOG_LINE_COUNT <= 10) {
+    LOGLINE_SECTION_COUNT = LOG_LINE_COUNT - 1 
+}
+let highlightLogStartIndex = 300
+
 const BLACK = '#000'
 const GREY = '#bfbfbf'
 const RED = '#ff4d4f'
@@ -22,117 +30,57 @@ var logs = Array.from({length: LOG_LINE_COUNT}).map(() => {
 }).sort((a, b) => {
     return a.time - b.time
 })
-var highlightLogIndex = 53 
 
 var barHeight = canvas.height
-var barWidth = 100 * ratio
+var barCssHeight = parseInt(canvas.style.height)
+var barWidth = 30 * ratio
 var timeTextWidth = 50 * ratio
 var $tooltip = document.getElementById('tooltip')
 
-document.addEventListener('mousewheel', (e) => {
-    // e.preventDefault()
-    // e.stopPropagation()
-    // if (e.target.id === 'can') {
-    //     lastFrameTime = Date.now()
-    //     if (e.wheelDelta > 0) {
-    //         augment(e.offsetY)
-    //     } else {
-    //         // 还能缩，否则停止reduce，减少不必要的redraw
-    //         if (timeBoundStart > initStart || timeBoundEnd < initEnd) {
-    //             reduce(e.offsetY)
-    //         }
-    //     }
-    // }
-})
-document.addEventListener('mousedown', (e) => {
-    // if (e.target.id === 'can') {
-    //     mouseDown = true
-    //     mouseLastMovePoint = {
-    //         x: e.offsetX,
-    //         y: e.offsetY
-    //     }
-    // }
-})
-document.addEventListener('mousemove', (e) => {
-    if (e.target.id === 'can') {
-        let hoveredLogIndex = getMouseHoveredLogIndex(e.offsetY)
-        //TODO index超出logs长度
-        if (hoveredLogIndex !== null) {
-            clearCanvas()
-            showToolTip(hoveredLogIndex, logs[hoveredLogIndex])
-            draw()
+if (LOG_LINE_COUNT < 2 || LOG_LINE_COUNT < LOGLINE_SECTION_COUNT + 1) {
+    console.error('LOG_LINE_COUNT is not valid')
+} else {
+    draw()
+    showHighlightFrame()
+    document.addEventListener('mousemove', (e) => {
+        redraw()
+        showHighlightFrame()
+        if (e.target.id === 'can') {
+            let hoveredLogIndex = getMouseHoveredLogIndex(e.offsetY)
+            if (hoveredLogIndex !== null) {
+                showToolTip(hoveredLogIndex, logs[hoveredLogIndex])
+            }
         }
-    }
-})
-document.addEventListener('mouseup', (e) => {
-    // mouseDown = false
-})
-document.addEventListener('mouseover', (e) => {
-    if (e.target.id === 'can') {
-        // console.log('y' + e.offsetY)
-    }
-})
-
-// NOTE: canvas.style.height需要比MOUSE_TOLERENCE大
-function getMouseHoveredLogIndex (offsetY) {
-    const MOUSE_TOLERENCE = 1 // 上下像素容错范围
-    var lineCssSpan = parseInt(canvas.style.height) / LOG_LINE_COUNT
-    var outOfLineRange = offsetY % lineCssSpan // 鼠标hover处距离某条line多少高度
-    if (outOfLineRange < MOUSE_TOLERENCE) {
-        // 接近上一条line
-        return parseInt(offsetY / lineCssSpan)
-    } else if (outOfLineRange > lineCssSpan - MOUSE_TOLERENCE) {
-        // 接近下一条line
-        return parseInt(offsetY / lineCssSpan) + 1
-    } else {
-        // 不接近任何一条line
-        return null
-    }
+    })
+    document.addEventListener('mouseover', (e) => {
+        redraw()
+        showHighlightFrame()
+        if (e.target.id === 'can') {
+            let hoveredLogIndex = getMouseHoveredLogIndex(e.offsetY)
+            if (hoveredLogIndex !== null) {
+                showToolTip(hoveredLogIndex, logs[hoveredLogIndex])
+            }
+        }
+    })
+    document.addEventListener('click', (e) => {
+        if (e.target.id === 'can') {
+            let hoveredLogIndex = getMouseHoveredLogIndex(e.offsetY)
+            if (hoveredLogIndex !== null) {
+                highlightLogStartIndex = hoveredLogIndex
+                redraw()
+                showHighlightFrame()
+            }
+        }
+    })
 }
-
-draw()
-function move(cssMove) {
+function redraw () {
+    $tooltip.style.display = 'none'
     clearCanvas()
-    modiTimeBoundsByMove(cssMove)
-    console.warn('redraw')
     draw()
 }
-function _calculateChangePointTime(offsetY) {
-    // 计算放大点对应的时间值
-    let cssRatio = offsetY / parseInt(canvas.style.height)
-    let timeTotalDiff = timeBoundEnd - timeBoundStart
-    let timeDiff = timeTotalDiff * cssRatio
-    let changePointTime = parseInt(timeDiff + timeBoundStart)
-    return changePointTime
-}
+
 function clearCanvas() {
     context.clearRect(0, 0, canvas.width, canvas.height)
-}
-
-/**
- * 
- * @param {Int} cssMove 可正负，正表示图片整体向上移动，负反之
- */
-function modiTimeBoundsByMove(cssMove) {
-    let timeDiff = timeBoundEnd - timeBoundStart
-    let cssDiff = parseInt(canvas.style.height)
-    let timeMove = parseInt(timeDiff * cssMove / cssDiff)
-    if (Math.abs(timeMove) < 1 ) {
-        timeMove = cssMove < 0 ? -1 : 1 // 如果timeMove已经很小了，那么最小1毫秒步进
-    }
-    // 判断移动后的时间没有超过头尾限制
-    let newTimeBoundStart = timeBoundStart + timeMove
-    let newTimeBoundEnd = timeBoundEnd + timeMove
-    if (newTimeBoundStart < initStart) {
-        timeBoundStart = initStart
-        timeBoundEnd = initStart + timeDiff
-    } else if (newTimeBoundEnd > initEnd) {
-        timeBoundEnd = initEnd
-        timeBoundStart = initEnd - timeDiff
-    } else {
-        timeBoundStart = timeBoundStart + timeMove
-        timeBoundEnd = timeBoundEnd + timeMove
-    }
 }
 
 function drawLogLine(width, height, color) {
@@ -158,25 +106,43 @@ function drawBoldLine (startPoint, width, height, color) {
     context.fillRect(startPoint.x, startPoint.y, width, height)
 }
 
+/**
+ * 在startPoint与endPoint构成的长方形外面套frameLineWidth厚度的边框
+ * @param {*} startPoint 左上角的坐标
+ * @param {*} endPoint 右下角的坐标
+ * @param {*} frameLineWidth 
+ */
+function drawFrame (startPoint, endPoint, frameLineWidth, color) {
+    context.fillStyle = color || '#eb2f96'
+    let frameWidth = endPoint.x - startPoint.x + 2 * frameLineWidth
+    let frameHeight = endPoint.y - startPoint.y + 2 * frameLineWidth
+    // 上边框
+    context.fillRect(startPoint.x - frameLineWidth, startPoint.y - frameLineWidth, frameWidth, frameLineWidth)
+    // 左边框
+    context.fillRect(startPoint.x - frameLineWidth, startPoint.y - frameLineWidth, frameLineWidth, frameHeight)
+    // 右边框
+    context.fillRect(endPoint.x, startPoint.y - frameLineWidth, frameLineWidth, frameHeight)
+    // 下边框
+    context.fillRect(startPoint.x - frameLineWidth, endPoint.y, frameWidth, frameLineWidth)
+}
+
 function draw() {
     // 画bar的border
     drawLine({ x: timeTextWidth, y: 0 }, { x: timeTextWidth, y: barHeight })
-    var lineHeightSpan = barHeight / LOG_LINE_COUNT
+    drawLine({ x: timeTextWidth + barWidth, y: 0 }, { x: timeTextWidth + barWidth, y: barHeight })
+    var lineCanvasHeightSpan = barHeight / (LOG_LINE_COUNT - 1)
     // 画logline
     logs.forEach(function(log, index) {
         let color = log.logType.displayColor
-        drawLogLine(50 * ratio, lineHeightSpan * index, color)
+        drawLogLine(barWidth, lineCanvasHeightSpan * index, color)
     })
-    // 画框
-    // if (highlightLogIndex <= index && index < highlightLogIndex + HIGH_LIGHT_LOG_LINE_COUNT ) {
-    //     color = RED
-    // }
+
     // 画时间刻度
     var timeDiff = logs[LOG_LINE_COUNT - 1].time - logs[0].time
     var timeSpan = timeDiff / LOGLINE_SECTION_COUNT
-    var timeStamps = Array.from({length: LOGLINE_SECTION_COUNT + 1}).map((i, index) => {
+    var timeStamps = Array.from({length: LOGLINE_SECTION_COUNT}).map((i, index) => {
         return parseInt(index * timeSpan) + logs[0].time
-    })
+    }).concat(logs[LOG_LINE_COUNT - 1].time)
     timeStamps.forEach((time, index) => {
         let height
         if (index === 0) {
@@ -186,18 +152,50 @@ function draw() {
         } else {
             height = index * (barHeight / LOGLINE_SECTION_COUNT)
         }
-        drawText(stringify(new Date(time)), 0, height)
+        drawText(stringify(new Date(time), true), 10 * ratio, height)
     })
 }
 
+function showHighlightFrame () {
+    var lineCanvasHeightSpan = barHeight / (LOG_LINE_COUNT - 1)
+    const FRAME_LINE_WIDTH = 3 * ratio
+    const FRAME_COLOR = '#fa541c'
+    let logStartIndex = highlightLogStartIndex < LOG_LINE_COUNT - HIGH_LIGHT_LOG_LINE_COUNT - 1 ? highlightLogStartIndex : (LOG_LINE_COUNT - HIGH_LIGHT_LOG_LINE_COUNT - 1)
+    let frameStartPoint = {
+        x: timeTextWidth,
+        y: logStartIndex * lineCanvasHeightSpan
+    }
+    let frameEndPoint = {
+        x: timeTextWidth + barWidth,
+        y: (logStartIndex + HIGH_LIGHT_LOG_LINE_COUNT) * lineCanvasHeightSpan
+    }
+    drawFrame(frameStartPoint, frameEndPoint, FRAME_LINE_WIDTH, FRAME_COLOR)
+}
+
 function showToolTip (index, log) {
-    var lineHeightSpan = barHeight / LOG_LINE_COUNT
+    var lineCanvasHeightSpan = barHeight / (LOG_LINE_COUNT - 1)
     drawBoldLine({
         x: timeTextWidth,
-        y: lineHeightSpan * index
-    }, 50 * ratio, 5, BLUE)
+        y: lineCanvasHeightSpan * index - 3
+    }, barWidth, 6, BLUE)
     // 展示tooltip
-    $tooltip.innerHTML = '时间: ' + stringify(new Date(log.time))
+    $tooltip.innerHTML = '时间: ' + stringify(new Date(log.time)) + '</br>日志类型: ' + log.logType.logTypeName
     $tooltip.style.display = 'block'
-    $tooltip.style.top = lineHeightSpan * index / ratio
+    $tooltip.style.top = lineCanvasHeightSpan * index / ratio
+}
+
+function getMouseHoveredLogIndex (offsetY) {
+    const MOUSE_TOLERENCE = 1 // 上下像素容错范围
+    var lineCssHeightSpan = barCssHeight / (LOG_LINE_COUNT - 1)
+    var outOfLineRange = offsetY % lineCssHeightSpan // 鼠标hover处距离某条line多少高度
+    if (outOfLineRange < MOUSE_TOLERENCE) {
+        // 接近上一条line
+        return parseInt(offsetY / lineCssHeightSpan)
+    } else if (outOfLineRange > lineCssHeightSpan - MOUSE_TOLERENCE) {
+        // 接近下一条line
+        return parseInt(offsetY / lineCssHeightSpan) + 1
+    } else {
+        // 不接近任何一条line
+        return null
+    }
 }
